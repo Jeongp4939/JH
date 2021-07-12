67f9a3f3a830fefd4622460a68560aed62f87bdc
Jeongp4939 <jeongp12@gmail.com>
2021-07-12 15:07:17 +0900

    7/12 commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,12 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
 def hello_world(request):
-    if request.method == 'POST':
+    if request.method == 'POST':    # POST CREATE DATA
 
         temp = request.POST.get('hello_world_input')
 
@@ -14,11 +16,9 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        hello_world_list = HelloWorld.objects.all()    # sql의 셀렉트문
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))  # accountapp 의 hello_world 라는 앱으로 가라는 뜻
 
-        return render(request, 'accountapp/hello_world.html',
-                      context={'hello_world_list': hello_world_list})
     else:
-        hello_world_list = HelloWorld.objects.all()
+        hello_world_list = HelloWorld.objects.all()    # sql의 셀렉트문 # GET VIEW DATA
         return render(request, 'accountapp/hello_world.html',
                       context={'hello_world_list': hello_world_list})
