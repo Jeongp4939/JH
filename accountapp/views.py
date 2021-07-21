326f3fc00c3f1e032b773136c653a5daf8b7a325
Jeongp4939 <jeongp12@gmail.com>
2021-07-21 10:50:42 +0900

    7/21 commit 4

--- accountapp/views.py
+++ accountapp/views.py
@@ -9,12 +9,14 @@ from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountCreationForm
 from accountapp.models import HelloWorld
 
+
 @login_required(login_url=reverse_lazy('accountapp:login'))
 def hello_world(request):
-    if request.method == 'POST':    # POST CREATE DATA
+    if request.method == 'POST':  # POST CREATE DATA
 
         temp = request.POST.get('hello_world_input')
 
@@ -25,9 +27,10 @@ def hello_world(request):
         return HttpResponseRedirect(reverse('accountapp:hello_world'))  # accountapp 의 hello_world 라는 앱으로 가라는 뜻
 
     else:
-        hello_world_list = HelloWorld.objects.all()    # sql의 셀렉트문 # GET VIEW DATA
+        hello_world_list = HelloWorld.objects.all()  # sql의 셀렉트문 # GET VIEW DATA
         return render(request, 'accountapp/hello_world.html',
                       context={'hello_world_list': hello_world_list})
+
 
 class AccountCreateView(CreateView):
     model = User
@@ -41,18 +44,23 @@ class AccountDetailView(DetailView):
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
-@method_decorator(login_required(login_url=reverse_lazy('accountapp:login')), 'get')
-@method_decorator(login_required(login_url=reverse_lazy('accountapp:login')), 'post')
+
+has_ownership = [login_required, account_ownership_required]
+
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
     context_object_name = 'target_user'
-    success_url = reverse_lazy('accountapp:hello_world')    # 아직 detail 로는 못돌아감(pk 값을 아직 안넣음)
+    success_url = reverse_lazy('accountapp:hello_world')  # 아직 detail 로는 못돌아감(pk 값을 아직 안넣음)
     template_name = 'accountapp/update.html'
 
-@method_decorator(login_required(login_url=reverse_lazy('accountapp:login')), 'get')
-@method_decorator(login_required(login_url=reverse_lazy('accountapp:login')), 'post')
-class AccountDeleteView(DeleteView):        # 탈퇴시엔 form이 필요 없으므로 일단 제외함
+
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
+class AccountDeleteView(DeleteView):  # 탈퇴시엔 form이 필요 없으므로 일단 제외함
     model = User
     context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:hello_world')
