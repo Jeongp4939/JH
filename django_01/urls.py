dbc5909522f3a6dbb158833f202025688e781d16
Jeongp4939 <jeongp12@gmail.com>
2021-07-22 15:14:41 +0900

    7/22 commit 2

--- django_01/urls.py
+++ django_01/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accountapp/', include('accountapp.urls'))
+    path('accountapp/', include('accountapp.urls')),
+    path('porfiles/', include('profileapp.urls')),
 ]
