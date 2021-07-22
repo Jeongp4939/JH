019bee8ed3023d8ecee1092800016dd46bbeab1e
Jeongp4939 <jeongp12@gmail.com>
2021-07-22 15:52:40 +0900

    7/22 commit 3

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='profile')     # user 라는 객체가 사라졌을 때 이 모델이 보일 정책
+
+    image = models.ImageField(upload_to='profile/', null=True)
+    nickname = models.CharField(max_length=30, unique=True)
+    message = models.CharField(max_length=200, null=True)
+
